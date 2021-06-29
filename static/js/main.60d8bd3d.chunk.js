6ead53d25e848970ef3294f25aca490f8ed72a89
venom_ss <venom_ss@protonmail.com>
2021-06-29 10:19:06 +0400

    Updates

---
+++ static/js/main.60d8bd3d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-gh"]=this["webpackJsonpreact-gh"]||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var r=c(1),s=c.n(r),n=c(3),a=c.n(n),i=c(0);var h=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("h1",{children:"This is fresh react app"})})};a.a.render(Object(i.jsx)(s.a.StrictMode,{children:Object(i.jsx)(h,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.60d8bd3d.chunk.js.map
